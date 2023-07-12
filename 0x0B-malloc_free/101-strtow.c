f653ac63d92cbc5ad6e353495dc6a848b71d701f
mankind71 <makindetolulope37@gmail.com>
2023-07-12 09:09:44 +0100

    malloc free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -13,23 +13,29 @@ char **strtow(char *str)
 {
 	char **arr;
 	int i, num_of_words;
+	char *word;
 
 	if (str == NULL)
 		return (NULL);
-	for (i = 0; str[i]; i++)
-	{
-		if (str[i] == ' ' && str[i-1] != ' ')
-			num_of_words++;
-	}
-	num_of_words++;
-	printf("%d\n",num_of_words);
-	arr = malloc(sizeof(char *) * (num_of_words + 1));
-	if (arr == NULL)
+	if (str == "")
 		return (NULL);
 	for (i = 0; str[i]; i++)
 	{
-		if (str[i] == ' ' && str[i-1] != ' ')
-			
+		if (str[i] == ' ' && str[i - 1] != ' ')
+			num_of_words++;
+	}
+	num_of_words++;
+	printf("%d\n", num_of_words);
+	arr = malloc(sizeof(char *) * (num_of_words));
+	if (arr == NULL)
+		return (NULL);
+	
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] == ' ')
+			continue;
+
+		if (str[i] == ' ' && str[i - 1] != ' ')
 	}
 	printf("\n");
 	return (arr);
