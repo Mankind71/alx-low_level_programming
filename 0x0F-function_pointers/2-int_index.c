3caeba3c251f45aa26077ab24357f00692218dab
mankind71 <makindetolulope37@gmail.com>
2023-07-20 06:47:16 +0100

    int index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,15 +2,25 @@
 
 /**
  * int_index - searches for int
- * 
+ *
  * @array: starting address
  * @size: size of array
  * @cmp: pointer to fnx to compare
- * 
- * Return: -1 if no match of size <= 0, 
+ *
+ * Return: -1 if no match of size <= 0,
 */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    
+	int a;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (a = 0; a < size; a++)
+	{
+		if (cmp(array[a]))
+			return (a);
+	}
+	return (-1);
 }
