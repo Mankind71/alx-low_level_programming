d693ab09aa8f26ad41305f62ebaa010fe298f35f
mankind71 <makindetolulope37@gmail.com>
2023-07-23 02:20:45 +0100

    prints all args with variadic

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,15 +18,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ap, int));
-		if (i < n - 1)
-		{
-			if (separator != NULL)
-				printf("%s", separator);
-		}
-		else
-		{
-			printf("\n");
-		}
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
 	}
+	printf("\n");
 	va_end(ap);
 }
