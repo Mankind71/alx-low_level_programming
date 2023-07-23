16737ac2a348820f65a9b079cfb80af56bcedea1
mankind71 <makindetolulope37@gmail.com>
2023-07-23 02:24:54 +0100

    sum all args with variadic

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,33 +1,28 @@
-#include "variadic_functions.h"
 #include <stdarg.h>
 
 /**
- * sum_them_all - function that sums all its parameters
+ * sum_them_all - function that sums its parameters
  *
- * @n: number of paramters passed to the function.
+ * @n: the number of variable params
  * @...: A variable number of paramters to calculate the sum of.
  *
- * Return: If n == 0 - 0.
- *         Otherwise - the sum of all parameters.
+ * Return: its sum or 0
  */
 
 int sum_them_all(const unsigned int n, ...)
-
 {
+	unsigned int sum, i;
 	va_list ap;
-	unsigned int a, b = 0;
 
+	sum = 0;
+	if (n == 0)
+		return 0;
+	
 	va_start(ap, n);
-
-	for (a = 0; a < n; a++)
-		b += va_arg(ap, int);
-
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ap, int);
+	}
 	va_end(ap);
-
-	return (b);
+	return (sum);
 }
-
-
-/*in this function, b += va_arg(ap, int) means add*/
-/* b to va_arg(ap, int) and call the new value b*/
-/* and so on. that creates a loop for every input*/
