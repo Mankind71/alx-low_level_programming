5918bedc0cbc1dff1a3ed14c885139ed0dce65bc
mankind71 <makindetolulope37@gmail.com>
2023-08-01 22:04:13 +0100

    4 free node

--- 0x13-more_singly_linked_lists/4-free_list.c
+++ 0x13-more_singly_linked_lists/4-free_list.c
@@ -1,19 +1,18 @@
 #include "lists.h"
 
 /**
- * free_list - a function that adds a new node
+ * free_listint - a function that adds a new node
  * at the end of a list_t list.
  * @head: the pointer to the pointer to the head
  */
 
-void free_list(list_t *head)
+void free_listint(listint_t *head)
 {
-	list_t *temp;
+	listint_t *temp;
 
 	while (head)
 	{
 		temp = head->next;
-		free(head->str);
 		free(head);
 		head = temp;
 	}
