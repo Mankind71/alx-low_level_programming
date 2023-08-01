97868f626e3f87b5da53c2d158205c5aef44d2e4
mankind71 <makindetolulope37@gmail.com>
2023-08-01 21:52:36 +0100

    1 list int len

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * listint_len - a function that returns num of elem in a list
+ * @h: the pointer to the list
+ * Return: number of list elems
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	if (h == NULL)
+		return (0);
+
+	return (1 + list_len(h->next));
+}
