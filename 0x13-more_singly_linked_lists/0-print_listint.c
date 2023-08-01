082165a4d853fcf804dc1cd23ea0fa3f7af8c58a
mankind71 <makindetolulope37@gmail.com>
2023-08-01 21:48:26 +0100

    print list int

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * print_listint - a function that prints all elems in list
+ * @h: the pointer to the list to print
+ * Return: number of node
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	if (h == NULL)
+		return (0);
+	if (h->n)
+		printf("%i\n", h->n);
+
+	return (1 + print_listint(h->next));
+}
---
+++ 0x13-more_singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_node - a function that adds a new node
+ * at the beginning of a list_t list.
+ * @head: the pointer to the pointer to the head
+ * @str: string to copy to the first elem
+ * Return: pointer to head node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *ptr = NULL;
+	unsigned int x = 0;
+
+	while (str[x])
+		x++;
+
+	ptr = malloc(sizeof(list_t));
+	if (!ptr)
+		return (0);
+
+	ptr->next = (*head);
+	ptr->str = strdup(str);
+	ptr->len = x;
+	(*head) = ptr;
+
+	return (*head);
+}
