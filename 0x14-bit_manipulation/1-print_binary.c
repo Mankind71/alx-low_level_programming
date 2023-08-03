0489e6a1a500292053518b2d6cee7b76add2329f
mankind71 <makindetolulope37@gmail.com>
2023-08-03 22:49:00 +0100

    convert num to bin

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number.
+ *
+ * @n: number to print
+ */
+
+
+void print_binary(unsigned long int n)
+{
+	int i, num_bits, bit;
+	int leadingZeros = 1;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+	num_bits = 32;
+
+	for (i = num_bits - 1; i >= 0; i--)
+	{
+		bit = (n >> i) & 1;
+		if (bit == 1)
+		{
+			leadingZeros = 0;
+			printf("%d", bit);
+		} else if (!leadingZeros)
+		{
+			printf("%d", bit);
+		}
+	}
+	printf("\n");
+}
