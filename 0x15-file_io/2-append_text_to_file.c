72e36908b3021e47670ffdb0dd059d611f02d92c
mankind71 <makindetolulope37@gmail.com>
2023-08-25 10:36:40 +0100

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,22 +11,22 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd, len;
+	int fd, len;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        for (len = 0; text_content[len];)
-            len++;
-    }
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-    fd = open(filename, O_WRONLY |O_APPEND);
-    if (fd == -1)
-        return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
 
-    write(fd, text_content, len);
-    close(fd);
-    return (1);
+	write(fd, text_content, len);
+	close(fd);
+	return (1);
 }
