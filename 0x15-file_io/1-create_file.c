82d440ad4153229ba9c74b5ed3f76a98dc5cc287
mankind71 <makindetolulope37@gmail.com>
2023-08-25 10:56:58 +0100

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 		for (txt_len = 0; text_content[txt_len];)
 			txt_len++;
 	}
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
 	{
