f1ce16a1da3f7f0e5da76a0b8259eb30fafd9cd3
mankind71 <makindetolulope37@gmail.com>
2023-08-25 10:33:42 +0100

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,8 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, txt_len, bytes_written;
+	int fd, txt_len;
+	ssize_t bytes_written;
 
 	if (filename == NULL)
 		return (-1);
