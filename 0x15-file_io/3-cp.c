c16805c49cb6b1466d2ddfc18f112f7f8780edaf
mankind71 <makindetolulope37@gmail.com>
2023-08-25 16:44:05 +0100

    copied from 1 to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,30 +1,44 @@
 #include "main.h"
 
 /**
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor
+ */
+
+void close_file(int fd)
+{
+	int c;
+
+	c = close(fd);
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
  * main - copies content of a file to another
- * 
- * @file_from: file to copy from
- * @file_to: file to copy to
-*/
+ *
+ * @ac: args count
+ * @av: args pointer
+ *
+ * Return: 1 on success
+ */
 
 int main(int ac, char **av)
 {
-    int fd1, fd2, rdfd1, wrfd2;
+	int fd1, fd2, rdfd1, wrfd2;
 	char *ch;
-    int chsize = 1024;
-    
-    if (ac != 3)
-    {
-        dprintf(2, "Usage: cp %s %s\n", av[1], av[2]);
-        exit(97);
-    }
+	int chsize = 1024;
+
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 	fd1 = open(av[1], O_RDONLY);
-    if (fd1 == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-        exit(98);
-    }
-    ch = malloc(sizeof(char) * chsize);
+	ch = malloc(sizeof(char) * chsize);
 
 	if (ch == NULL)
 	{
@@ -32,37 +46,23 @@ int main(int ac, char **av)
 			"Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-    rdfd1 = read(fd1, ch, chsize);
-    if (rdfd1 == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-        exit(98);
-    }
-    if(close(fd1) == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
-        exit(100);
-    }    
-    
-	fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-    if (fd2 == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-        exit(99);
-    }
-    wrfd2 = write(fd2, ch, rdfd1);
-    if (wrfd2 == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-        exit(99);
-    }
-    
+	rdfd1 = read(fd1, ch, chsize);
+	if (fd1 == -1 || rdfd1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
 
-    if(close(fd2) == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
-        exit(100);
-    }
-    free(ch);
-    return (1);
+	fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	wrfd2 = write(fd2, ch, rdfd1);
+	if (fd2 == -1 || wrfd2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+
+	free(ch);
+	close(fd1);
+	close(fd2);
+	return (0);
 }
