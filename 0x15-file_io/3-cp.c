339568813c81b2aead84889b67f51d859964d14b
mankind71 <makindetolulope37@gmail.com>
2023-09-07 19:34:53 +0100

    fixes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,6 +18,21 @@ void close_file(int fd)
 }
 
 /**
+ * check_read - check read values.
+ * @fd: The file descriptor
+ * @val: av[1]
+ */
+
+void check_read(int fd, char *val)
+{
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", val);
+		exit(98);
+	}
+}
+
+/**
  * main - copies content of a file to another
  *
  * @ac: args count
@@ -46,14 +61,11 @@ int main(int ac, char **av)
 		exit(99);
 	}
 	rdfd1 = read(fd1, ch, chsize);
+	check_read(fd1, av[1]);
+	check_read(rdfd1, av[1]);
 	fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	while (rdfd1 > 0)
 	{
-		if (fd1 == -1 || rdfd1 == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-			exit(98);
-		}
 		wrfd2 = write(fd2, ch, rdfd1);
 		if (fd2 == -1 || wrfd2 == -1)
 		{
@@ -62,6 +74,7 @@ int main(int ac, char **av)
 			exit(99);
 		}
 		rdfd1 = read(fd1, ch, chsize);
+		check_read(rdfd1, av[1]);
 		fd2 = open(av[2], O_WRONLY | O_APPEND);
 	}
 	free(ch);
